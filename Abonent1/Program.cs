db7f496da94356e1667c0a78f7d9a3f3688484f7
bartekk3007 <149260538+bartekk3007@users.noreply.github.com>
2024-05-18 23:54:28 +0200

    Add files via upload

---
+++ Abonent1/Program.cs
@@ -0,0 +1,46 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace abonent1
+{
+    class MyConsumer : DefaultBasicConsumer
+    {
+        public MyConsumer(IModel model) : base(model) { }
+        public override void HandleBasicDeliver(string consumerTag, ulong deliveryTag, bool
+            redelivered, string exchange, string routingKey, IBasicProperties properties,
+            ReadOnlyMemory<byte> body)
+        {
+            var message = Encoding.UTF8.GetString(body.ToArray());
+            Console.WriteLine($"Odebrano - \"{message}\"");
+        }
+    }
+
+    internal class Abonent1
+    {
+        static void Main(string[] args)
+        {
+            Thread.Sleep(500);
+            Console.WriteLine("Abonent 1");
+            var factory = new ConnectionFactory()
+            {
+                UserName = "guest",
+                Password = "guest",
+                HostName = "localhost",
+                VirtualHost = "/"
+            };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare("abc", "topic");
+                var queueName6 = channel.QueueDeclare().QueueName;
+                channel.QueueBind(queueName6, "abc", "abc.#");
+                var consumer = new MyConsumer(channel);
+                channel.BasicConsume(queueName6, true, consumer);
+                Task.Run(() => Console.ReadKey()).Wait();
+            }
+        }
+    }
+}
---
+++ Wydawca/Program.cs
@@ -0,0 +1,45 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wydawca
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Wydawca");
+            Random r = new Random();
+            var factory = new ConnectionFactory()
+            {
+                UserName = "guest",
+                Password = "guest",
+                HostName = "localhost",
+                VirtualHost = "/"
+            };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare("abc", "topic");
+                for (int i = 0; i < 10; i++)
+                {
+                    var body = Encoding.UTF8.GetBytes($"wiadomość {i}");
+                    string kanal;
+                    if (i % 2 == 0)
+                    {
+                        kanal = "abc.def";
+                    }
+                    else
+                    {
+                        kanal = "abc.xyz";
+                    }
+                    channel.BasicPublish("abc", kanal, null, body);
+                    Console.WriteLine($"Wysłano {i} - kanał - {kanal}");
+                }
+                Task.Run(() => Console.ReadKey()).Wait();
+            }
+        }
+    }
+}
